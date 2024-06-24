179c7a40359c06c380863d5d562b29f6c9f30cff
Irfan Deen <e1113758@u.nus.edu>
2024-06-24 20:52:32 +0800

    Init commit

---
+++ encrypt.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+helper() {
+	mode=$1
+	inputfile=''
+	outputfile=''
+	keyfile=''
+
+	# Get positional arguments
+	while [[ $# -gt 0 ]]; do
+		case $1 in
+		--in | -i)
+			inputfile=$2
+			echo "input file is $inputfile"
+			;;
+		--out | -o)
+			outputfile=$2
+			echo "output file is $outputfile"
+			;;
+		--key | -k)
+			keyfile=$3
+			echo "key file is $keyfile"
+			;;
+		--help | -h)
+			echo "Usage:"
+			echo "	encrypt --in inputfile --out outputfile [optional] --key keyfile"
+			echo "	decrypt --in encryptedfile --out decryptedfile [optional] --key keyfile"
+			echo "	genkey keyfilename"
+			kill -INT $$
+			;;
+		esac
+		shift
+	done
+
+	# Really Basic Error Handling
+	if [[ -z $inputfile || -z $outputfile ]]; then
+		echo "Input or output file missing, use --help to see usage."
+		kill -INT $$
+	fi
+
+	# Encrypt with key file or without
+	if [[ $mode -eq 0 ]]; then
+		if [[ -z $keyfile ]]; then
+			openssl enc -aes-256-cbc -pbkdf2 -in $inputfile -out $outputfile
+		else
+			openssl enc -aes-256-cbc -pbkdf2 -in $inputfile -out $outputfile -pass file:$keyfile
+		fi
+	else
+		if [[ -z $keyfile ]]; then
+			openssl aes-256-cbc -d -pbkdf2 -in $inputfile -out $outputfile
+		else
+			openssl aes-256-cbc -d -pbkdf2 -in $inputfile -out $outputfile -pass file:$keyfile
+		fi
+	fi
+}
+
+encrypt() {
+	helper 0 $@
+}
+
+decrypt() {
+	helper 1 $@
+}
+
+genkey() {
+	keyfile=$1
+	if [[ $# -eq 0 || $# -gt 1 || "$keyfile" == "--help" || "$keyfile" == "-h" ]]; then
+		echo "Usage:"
+		echo "	encrypt --in inputfile --out outputfile [optional] --key keyfile"
+		echo "	decrypt --in encryptedfile --out decryptedfile [optional] --key keyfile"
+		echo "	genkey keyfilename"
+	fi
+	
+	openssl rand 214 > $keyfile
+}
